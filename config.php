7c007e1a4a173d1f58d0230a32ba52b7fac32354
Alpha Chong <alphachongs@gmail.com>
2024-12-07 23:32:04 +0800

    comment sql status

--- config.php
+++ config.php
@@ -27,9 +27,11 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected successfully";
-}
+} 
+
+// else {
+//     echo "Connected successfully";
+// }
 
 // Create database if it doesn't exist
 $sql = "CREATE DATABASE IF NOT EXISTS $dbname";
