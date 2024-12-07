e2fd29d2373c992e8f24f1220ac61d465e10fcee
Alpha Chong <alphachongs@gmail.com>
2024-12-07 22:58:35 +0800

    Fixed asset path

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <title>Registration Form</title>
-    <link rel="stylesheet" href="/assets/style.css">
+    <link rel="stylesheet" href="/style.css">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
      <style>      
